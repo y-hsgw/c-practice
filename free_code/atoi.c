1340f5a55d113b68cfa3db8f439f3e321217cdab
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2023-02-08 21:49:23 +0900

    atoi関数学習

---
+++ free_code/atoi.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+  char str1 [] = "256";
+  char str2 [] = "2.56";
+  int number = atoi(str1);
+  double _float = atoi(str2);
+  printf("%d\n",number);
+  printf("%f\n",_float);
+  return 0;
+}
