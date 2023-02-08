a5eaffc65d46fe5e7512882227b9b59800071409
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2023-02-08 21:45:47 +0900

    文字列変数学習

---
+++ free_code/char.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+  char c = 'A';
+  char str [5] = {'J','0','H','N','\0'};
+  char str2 [] = "john";
+
+  printf("%c\n",c);
+  printf("%s\n",str);
+  printf("%s\n",str2);
+  return 0;
+}
