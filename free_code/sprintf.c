4f001ad3f844ab09905ae74966bccc949082f4f8
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2023-02-08 21:56:44 +0900

    sprintf関数学習

---
+++ free_code/sprintf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+  char str[20];
+  char str1[] = "Star";
+  char str2[] = "Wars";
+  int i = 3;
+  sprintf(str,"%s%s%d", str1,str2,i);
+  printf("%s\n", str);
+  return 0;
+}
