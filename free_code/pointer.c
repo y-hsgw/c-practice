28671e58fe403e7005c77aa34adadd5e27b4e387
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2023-02-09 22:00:57 +0900

    インデント修正

--- free_code/pointer.c
+++ free_code/pointer.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 int main(void) {
- int apple = 10;
- int *hoge;
- hoge = &apple;
+  int apple = 10;
+  int *hoge;
+  hoge = &apple;
   printf("%d\n", apple);
   printf("%d\n", *hoge);
   return 0;
