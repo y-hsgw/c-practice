bc530945ffa3bcad75c587df5e0acd0a03f75aa3
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2023-02-17 21:41:45 +0900

    多次元配列学習

---
+++ introduction/multi_array.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+int main(){
+  int ary[2][2];
+  ary[0][0] = 10;
+  ary[0][1] = 100;
+  ary[1][0] = 1000;
+  ary[1][1] = 10000;
+
+  printf("%d\n%d\n", ary[0][0], ary[0][1]);
+  printf("%d\n%d\n", ary[1][0], ary[1][1]);
+
+  return 0;
+}
