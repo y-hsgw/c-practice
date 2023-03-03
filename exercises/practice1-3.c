1d306f5e7c94ea5334528ca026c159c79c3a3bea
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2023-03-03 21:27:45 +0900

    練習問題1-3

---
+++ exercises/practice1-3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void first() { printf("%s\n", "ふるいけや"); }
+void second() { printf("%s\n", "かわずとびこむ"); }
+void third() { printf("%s\n", "みずのおと"); }
+
+int main() {
+  first();
+  second();
+  third();
+  return 0;
+}
